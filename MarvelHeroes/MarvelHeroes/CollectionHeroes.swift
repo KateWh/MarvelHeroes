c80b6fab0e503488678c6290fb841e561cb65c6a
ket <ketvitketvit@gmail.com>
2019-01-30 17:37:46 +0200

    start

--- MarvelHeroes/MarvelHeroes/CollectionHeroes.swift
+++ MarvelHeroes/MarvelHeroes/CollectionHeroes.swift
@@ -8,17 +8,13 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class CollectionHeroes: UITableViewController {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
 
 }
--- MarvelHeroes/MarvelHeroes/PrototypeHero.swift
+++ MarvelHeroes/MarvelHeroes/PrototypeHero.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import UIKit
+
+class PrototypeHero: UITableViewCell {
+
+
+
+    
+}
